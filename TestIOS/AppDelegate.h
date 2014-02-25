01922ca30eda63b651dadf8f823f5af4720d8ee7
moviles1 <moviles1@iMac-de-moviles1.local>
2014-02-25 13:32:26 -0800

    Initial Commit

---
+++ TestIOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestIOS
+//
+//  Created by moviles1 on 25/02/14.
+//  Copyright (c) 2014 moviles1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestIOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestIOS
+//
+//  Created by moviles1 on 25/02/14.
+//  Copyright (c) 2014 moviles1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
