b6a31bea54bbc76e683f92f7f504b4efe3f5c820
Noe Aguilar <noe@masfusion.com>
2014-02-25 17:36:59 -0800

    Se agrego el controlador Nueva

---
+++ TestIOS/NuevaViewController.h
@@ -0,0 +1,13 @@
+//
+//  NuevaViewController.h
+//  TestIOS
+//
+//  Created by moviles1 on 25/02/14.
+//  Copyright (c) 2014 moviles1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NuevaViewController : UIViewController
+
+@end
