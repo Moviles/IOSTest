388d4bfbc9e5462e1c2bdede47fc513e5bf997d0
Noe Aguilar <noe@masfusion.com>
2014-02-25 17:45:36 -0800

    Se agrego clase kdjkjdsk y se sdskdsj

---
+++ TestIOS/ViewControllerPrueba.m
@@ -0,0 +1,39 @@
+//
+//  ViewControllerPrueba.m
+//  TestIOS
+//
+//  Created by moviles1 on 25/02/14.
+//  Copyright (c) 2014 moviles1. All rights reserved.
+//
+
+#import "ViewControllerPrueba.h"
+
+@interface ViewControllerPrueba ()
+
+@end
+
+@implementation ViewControllerPrueba
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+        //sjksjkfsf djhfsjdkfjhdbfv jjhg
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
